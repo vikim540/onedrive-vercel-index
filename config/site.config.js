8cb8bb071e2995c4ef12cbf40762a8368703500c
Vikim <51993914+vikim540@users.noreply.github.com>
2022-11-29 17:43:05 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/加密文件/u-need-a-password'],
+  protectedRoutes: ['/加密文件'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:vikim_kee@outlook.com',
